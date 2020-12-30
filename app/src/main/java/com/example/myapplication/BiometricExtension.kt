2059f6ebdbc17dc7cd042c3a3353aa435bb58e95
sergeyk <s.komlach@gmail.com>
2020-12-30 16:42:41 +0200

    Test app updated

--- app/src/main/java/com/example/myapplication/BiometricExtension.kt
+++ app/src/main/java/com/example/myapplication/BiometricExtension.kt
@@ -3,11 +3,16 @@ package com.example.myapplication
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import dev.skomlach.biometric.compat.BiometricAuthRequest
+import dev.skomlach.biometric.compat.BiometricManagerCompat
 import dev.skomlach.biometric.compat.BiometricPromptCompat
 import dev.skomlach.biometric.compat.engine.AuthenticationFailureReason
 import dev.skomlach.biometric.compat.utils.logging.BiometricLoggerImpl
 
 fun Fragment.startBiometric(biometricAuthRequest: BiometricAuthRequest) {
+    if(!BiometricManagerCompat.hasEnrolled(biometricAuthRequest)){
+        BiometricManagerCompat.openSettings(requireActivity(), biometricAuthRequest)
+        return
+    }
     val start = System.currentTimeMillis()
     BiometricLoggerImpl.e("CheckBiometric.start() for $biometricAuthRequest")
     val biometricPromptCompat = BiometricPromptCompat.Builder(
