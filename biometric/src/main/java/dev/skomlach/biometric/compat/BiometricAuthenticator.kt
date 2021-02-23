3cf4b7ba07a8df32955573f5ed829a39cebef096
sergeyk <s.komlach@gmail.com>
2021-02-23 12:35:59 +0200

    Keep  moving to Kotlin

--- biometric/src/main/java/dev/skomlach/biometric/compat/BiometricAuthenticator.kt
+++ biometric/src/main/java/dev/skomlach/biometric/compat/BiometricAuthenticator.kt
@@ -1,9 +1,8 @@
-package dev.skomlach.biometric.compat;
+package dev.skomlach.biometric.compat
 
-public class BiometricAuthenticator {
-    public static final int TYPE_FACE = 4;
-    public static final int TYPE_FINGERPRINT = 1;
-    public static final int TYPE_IRIS = 2;
-    public static final int TYPE_NONE = 0;
-
+object BiometricAuthenticator {
+    const val TYPE_FACE = 4
+    const val TYPE_FINGERPRINT = 1
+    const val TYPE_IRIS = 2
+    const val TYPE_NONE = 0
 }
