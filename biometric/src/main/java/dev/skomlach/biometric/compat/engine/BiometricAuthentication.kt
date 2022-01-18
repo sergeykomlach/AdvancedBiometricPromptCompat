b16298be7cca42fcf5eb81496c3b3a803767e738
sergeyk <s.komlach@gmail.com>
2022-01-18 15:46:21 +0200

    Fixes for Samsung

--- biometric/src/main/java/dev/skomlach/biometric/compat/engine/BiometricAuthentication.kt
+++ biometric/src/main/java/dev/skomlach/biometric/compat/engine/BiometricAuthentication.kt
@@ -95,10 +95,11 @@ object BiometricAuthentication {
             allMethods.add(BiometricMethod.FACE_HUAWEI)
         }
         //Android biometric - Pie (9.0)
-        if (Build.VERSION.SDK_INT >= 28) {
-            allMethods.add(BiometricMethod.FACE_ANDROIDAPI)
-            allMethods.add(BiometricMethod.IRIS_ANDROIDAPI)
-        }
+        //TODO: Samsung enable this API, but implementation need to be finetuned
+//        if (Build.VERSION.SDK_INT >= 28) {
+//            allMethods.add(BiometricMethod.FACE_ANDROIDAPI)
+//            allMethods.add(BiometricMethod.IRIS_ANDROIDAPI)
+//        }
         moduleHashMap.clear()
         //launch in BG because for init needed about 2-3 seconds
         try {
