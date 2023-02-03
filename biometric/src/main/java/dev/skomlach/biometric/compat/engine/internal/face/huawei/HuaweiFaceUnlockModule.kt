652d9b8cd2de02ae1919c30b7257e75c17743d16
sergey.komlach <s.komlach@gmail.com>
2023-02-03 08:37:12 +0200

    new version released

--- biometric/src/main/java/dev/skomlach/biometric/compat/engine/internal/face/huawei/HuaweiFaceUnlockModule.kt
+++ biometric/src/main/java/dev/skomlach/biometric/compat/engine/internal/face/huawei/HuaweiFaceUnlockModule.kt
@@ -48,7 +48,7 @@ class HuaweiFaceUnlockModule(listener: BiometricInitListener?) :
             try {
                 huaweiFaceManagerLegacy = HuaweiFaceManagerFactory.getHuaweiFaceManager()
                 d("$name.huaweiFaceManagerLegacy - $huaweiFaceManagerLegacy")
-                if(HuaweiFaceRecognizeManager.shouldCheckCamera()) {
+                if(hasEnrolled() && HuaweiFaceRecognizeManager.shouldCheckCamera()) {
                     val cancellationSignal = CancellationSignal()
                     val checkTask = Runnable {
                         HuaweiFaceRecognizeManager.resetCheckCamera()
