5f757d4195fa1648d9962613cd07303aaa06707b
sergey.komlach <sergey.komlach@stickypassword.com>
2026-03-07 18:09:21 +0200

    Device model fixed

--- biometric/src/main/java/dev/skomlach/biometric/compat/utils/ScreenProtection.kt
+++ biometric/src/main/java/dev/skomlach/biometric/compat/utils/ScreenProtection.kt
@@ -32,13 +32,69 @@ import android.view.accessibility.AccessibilityNodeInfo
 import android.view.accessibility.AccessibilityNodeProvider
 import androidx.core.os.BuildCompat
 import androidx.core.view.ViewCompat
+import dev.skomlach.biometric.compat.R
 import dev.skomlach.biometric.compat.utils.logging.BiometricLoggerImpl
 import dev.skomlach.common.permissions.PermissionUtils
 import dev.skomlach.common.protection.A11yDetection
 
 object ScreenProtection {
-    //disable next features:
+    private data class ProtectedViewState(
+        val importantForAccessibility: Int,
+        val importantForAutofill: Int?,
+        val originalAccessibilityDelegate: View.AccessibilityDelegate?,
+        val accessibilityDataSensitive: Int?
+    )
 
+    private val blockingDelegate = object : View.AccessibilityDelegate() {
+        override fun sendAccessibilityEvent(host: View, eventType: Int) = Unit
+
+        override fun performAccessibilityAction(
+            host: View,
+            action: Int,
+            args: Bundle?
+        ): Boolean = false
+
+        override fun sendAccessibilityEventUnchecked(
+            host: View,
+            event: AccessibilityEvent
+        ) = Unit
+
+        override fun dispatchPopulateAccessibilityEvent(
+            host: View,
+            event: AccessibilityEvent
+        ): Boolean = false
+
+        override fun onPopulateAccessibilityEvent(
+            host: View,
+            event: AccessibilityEvent
+        ) = Unit
+
+        override fun onInitializeAccessibilityEvent(
+            host: View,
+            event: AccessibilityEvent
+        ) = Unit
+
+        override fun onInitializeAccessibilityNodeInfo(
+            host: View,
+            info: AccessibilityNodeInfo
+        ) = Unit
+
+        override fun addExtraDataToAccessibilityNodeInfo(
+            host: View,
+            info: AccessibilityNodeInfo,
+            extraDataKey: String,
+            arguments: Bundle?
+        ) = Unit
+
+        override fun onRequestSendAccessibilityEvent(
+            host: ViewGroup,
+            child: View,
+            event: AccessibilityEvent
+        ): Boolean = false
+
+        override fun getAccessibilityNodeProvider(host: View): AccessibilityNodeProvider? = null
+    }
+    //disable next features:
     //Screenshots
     //Accessibility Services
     //Android Oreo autofill in the app
@@ -103,7 +159,8 @@ object ScreenProtection {
             applyProtectionInView(
                 window?.findViewById(
                     Window.ID_ANDROID_CONTENT
-                ) ?: return
+                ) ?: return,
+                disableWindow
             )
         } catch (_: Exception) {
 
@@ -111,94 +168,102 @@ object ScreenProtection {
     }
 
     fun applyProtectionInView(
-        view: View
+        view: View,
+        disableView: Boolean = true
     ) {
         try {
             if (A11yDetection.hasWhiteListedService(view.context))
                 return
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                ViewCompat.setImportantForAutofill(
-                    view,
-                    View.IMPORTANT_FOR_AUTOFILL_NO_EXCLUDE_DESCENDANTS
-                )
-            }
-
-
-            ViewCompat.setImportantForAccessibility(
-                view,
-                ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_NO_HIDE_DESCENDANTS
-            )
-            if (BuildCompat.isAtLeastU()) {
-                view.setAccessibilityDataSensitive(View.ACCESSIBILITY_DATA_SENSITIVE_YES)
-            }
-            //Note: View.IMPORTANT_FOR_ACCESSIBILITY_NO_HIDE_DESCENDANTS doesn't have affect
-            //for 3rd party password managers
-            view.accessibilityDelegate = object : View.AccessibilityDelegate() {
-                override fun sendAccessibilityEvent(host: View, eventType: Int) {
-                }
-
-                override fun performAccessibilityAction(
-                    host: View,
-                    action: Int,
-                    args: Bundle?
-                ): Boolean {
-                    return false
-                }
-
-                override fun sendAccessibilityEventUnchecked(
-                    host: View,
-                    event: AccessibilityEvent
-                ) {
-                }
-
-                override fun dispatchPopulateAccessibilityEvent(
-                    host: View,
-                    event: AccessibilityEvent
-                ): Boolean {
-                    return false
-                }
-
-                override fun onPopulateAccessibilityEvent(
-                    host: View,
-                    event: AccessibilityEvent
-                ) {
-                }
-
-                override fun onInitializeAccessibilityEvent(
-                    host: View,
-                    event: AccessibilityEvent
-                ) {
-                }
-
-                override fun onInitializeAccessibilityNodeInfo(
-                    host: View,
-                    info: AccessibilityNodeInfo
-                ) {
-                }
-
-                override fun addExtraDataToAccessibilityNodeInfo(
-                    host: View,
-                    info: AccessibilityNodeInfo, extraDataKey: String,
-                    arguments: Bundle?
-                ) {
-                }
-
-                override fun onRequestSendAccessibilityEvent(
-                    host: ViewGroup, child: View,
-                    event: AccessibilityEvent
-                ): Boolean {
-                    return false
-                }
-
-                override fun getAccessibilityNodeProvider(host: View): AccessibilityNodeProvider? {
-                    return null
-                }
+            if (disableView) {
+                protectView(view)
+            } else {
+                rollbackView(view)
             }
         } catch (e: Exception) {
-            //not sure is exception can happens, but better to track at least
-            BiometricLoggerImpl.e(e, e.message)
+            //not sure is exception can happen, but better to track at least
+            BiometricLoggerImpl.e("ScreenProtection", e)
         }
     }
+    private fun protectView(view: View) {
+        val alreadyApplied = view.getTag(R.id.bio_tag_screen_protection_applied) as? Boolean ?: false
 
+        if (!alreadyApplied) {
+            saveOriginalState(view)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ViewCompat.setImportantForAutofill(
+                view,
+                View.IMPORTANT_FOR_AUTOFILL_NO_EXCLUDE_DESCENDANTS
+            )
+        }
+
+        ViewCompat.setImportantForAccessibility(
+            view,
+            ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_NO_HIDE_DESCENDANTS
+        )
+
+        if (BuildCompat.isAtLeastU()) {
+            view.setAccessibilityDataSensitive(View.ACCESSIBILITY_DATA_SENSITIVE_YES)
+        }
+
+        view.accessibilityDelegate = blockingDelegate
+        view.setTag(R.id.bio_tag_screen_protection_applied, true)
+    }
+
+    private fun rollbackView(view: View) {
+        val state = view.getTag(R.id.bio_tag_screen_protection_state) as? ProtectedViewState ?: run {
+            view.setTag(R.id.bio_tag_screen_protection_applied, false)
+            return
+        }
+
+        ViewCompat.setImportantForAccessibility(view, state.importantForAccessibility)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            state.importantForAutofill?.let {
+                ViewCompat.setImportantForAutofill(view, it)
+            }
+        }
+
+        if (BuildCompat.isAtLeastU()) {
+            state.accessibilityDataSensitive?.let {
+                view.setAccessibilityDataSensitive(it)
+            }
+        }
+
+        view.accessibilityDelegate = state.originalAccessibilityDelegate
+
+        view.setTag(R.id.bio_tag_screen_protection_applied, false)
+        view.setTag(R.id.bio_tag_screen_protection_state, null)
+    }
+
+    private fun saveOriginalState(view: View) {
+        val state = ProtectedViewState(
+            importantForAccessibility = view.importantForAccessibility,
+            importantForAutofill = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                view.importantForAutofill
+            } else {
+                null
+            },
+            originalAccessibilityDelegate = getCurrentAccessibilityDelegateCompat(view),
+            accessibilityDataSensitive = if (BuildCompat.isAtLeastU()) {
+                if(view.isAccessibilityDataSensitive) View.ACCESSIBILITY_DATA_SENSITIVE_YES else View.ACCESSIBILITY_DATA_SENSITIVE_NO
+            } else {
+                null
+            }
+        )
+
+        view.setTag(R.id.bio_tag_screen_protection_state, state)
+    }
+    @Suppress("DiscouragedPrivateApi", "PrivateApi")
+    private fun getCurrentAccessibilityDelegateCompat(view: View): View.AccessibilityDelegate? {
+        return try {
+            val field = View::class.java.getDeclaredField("mAccessibilityDelegate")
+            field.isAccessible = true
+            field.get(view) as? View.AccessibilityDelegate
+        } catch (_: Throwable) {
+            null
+        }
+    }
 }
