7beb22435c9a0c1d4bad27dd4f88eedfdf0c9433
sergey.komlach <sergey.komlach@stickypassword.com>
2026-04-12 00:07:43 +0300

    Reapply "Device model fixed"
    This reverts commit a89f718f8f32738159eb318ac3534d43d022c725.

--- biometric-custom-face-tf/DeepfakeFrameSequenceDetector.kt
+++ biometric-custom-face-tf/DeepfakeFrameSequenceDetector.kt
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (c) 2026 Sergey Komlach aka Salat-Cx65; Original project https://github.com/Salat-Cx65/AdvancedBiometricPromptCompat
+ *  All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package dev.skomlach.biometric.compat.engine.internal.face.tensorflow
 
 import android.content.res.AssetManager
@@ -34,7 +53,7 @@ class DeepfakeFrameSequenceDetector private constructor(
 
     companion object {
         private const val TAG = "DeepfakeSequenceDetector"
-        const val DEFAULT_MODEL_FILE = "tf_bio/deepfake_detection_model.tflite"
+        const val DEFAULT_MODEL_FILE = "deepfake_detection_model.tflite"
         const val DEFAULT_SEQUENCE_LENGTH = 10
         const val DEFAULT_INPUT_SIZE = 224
         const val DEFAULT_THRESHOLD = 0.5f
